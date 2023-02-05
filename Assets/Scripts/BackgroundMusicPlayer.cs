d5c8566a3d81ded6f02863136bd3b0cee61b47ca
Cudl <antto.vaatainen@gmail.com>
2023-02-05 14:35:42 +0200

    Audio

---
+++ Assets/Scripts/BackgroundMusicPlayer.cs
@@ -0,0 +1,16 @@
+
+using System;
+using RootieSmoothie;
+using RootieSmoothie.Audio;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicPlayer : Singleton<BackgroundMusicPlayer>
+{
+    [SerializeField] private AudioClip _gameplayMusic;
+
+    public void Start()
+    {
+        AudioManager.Instance.PlaySound(this, _gameplayMusic, true);
+    }
+}
